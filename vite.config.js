c47ff1ca95dc0356086f27f719b8815b71279ec1
Nikolay Topkaridi <enemycnt@gmail.com>
2024-08-12 13:28:49 +0300

    added bundler visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    visualizer({
+      emitFile: true,
+      filename: "stats.html",
+    }),
+  ],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
   },
